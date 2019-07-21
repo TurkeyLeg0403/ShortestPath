3bb129441ae32efdab7e7db17796c75b540b7c24
Takaki Otsu <turkeyleg0403@gmail.com>
2019-07-21 20:27:40 +0900

    add fin ver1.0

--- ShortestPath/TravelingSalesman.swift
+++ ShortestPath/TravelingSalesman.swift
@@ -2,40 +2,47 @@ import UIKit
 
 class TravelingSalesman {
     private var points: [CGPoint] = [];
-    private let exeCount: Int = 10^6;
+    private let exeCount: Int = 100000;
     private let tempreture: CGFloat = 10.0;
     private let cooler: CGFloat = 0.9;
     
-    init(points: [CGPoint] ) {
+    private let superView: UIView;
+    private let pathView: UIView;
+    
+    init(points: [CGPoint], superView: UIView, pathView:UIView) {
         self.points = points;
+        self.superView = superView;
+        self.pathView = pathView;
     }
     
     public func execute() -> [CGPoint] {
         do {
+            if self.points.isEmpty {
+                throw NSError(domain: "in execute, points is empty.", code: -1, userInfo: nil)
+            }
             var minPaths: [CGPoint] = self.points;
-            for _ in 0..<exeCount {
-                var random1 = Int.random(in: 0..<self.points.count)
-                var random2 = Int.random(in: 0..<self.points.count)
+            for i in 0..<exeCount {
+                var random1 = Int.random(in: 0..<self.points.count);
+                var random2 = Int.random(in: 0..<self.points.count);
                 if(random1 > random2){
                     swap(&random1, &random2)
                 }
-                print("random1 is \(random1)");
-                print("random2 is \(random2)");
-                let testPaths: [CGPoint] = self.reverseArray(array: self.points, head: random1, tail: random2) as! [CGPoint];
+                let testPaths: [CGPoint] = self.reverseArray(array: minPaths, head: random1, tail: random2) as! [CGPoint];
                 let dE = try self.sumDist(points: testPaths) - self.sumDist(points: minPaths);
-                print("dE is \(dE).");
                 if(dE > 0) {
                     let exp_dE = exp(dE * -1 / tempreture);
                     let random_dE = CGFloat.random(in: 0..<1);
                     if(exp_dE > random_dE) {
-                        print("not minimum, but chage paths.");
+//                        print("not minimum, but change paths.");
                         minPaths = testPaths;
                     }
                 } else {
+//                    print("change paths. \(i)");
                     minPaths = testPaths;
                 }
             }
-            print("minPaths is \(minPaths)");
+//            print("minPaths is \(minPaths)");
+            print("sumDist is \(try self.sumDist(points: minPaths))");
             return minPaths;
         } catch {
             print("Error: in TravelingSalesman, execute().")
@@ -43,9 +50,20 @@ class TravelingSalesman {
         }
     }
     
+    public func drawPathLines(resultPaths: [CGPoint], superView: UIView, pathView: UIView) {
+        superView.viewWithTag(999)?.removeFromSuperview();
+        let draw2D: Draw2D = Draw2D(frame: pathView.frame);
+        draw2D.tag = 999;
+        draw2D.isOpaque = false;
+        draw2D.backgroundColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0);
+        draw2D.getResultPaths(resultPaths: resultPaths);
+        superView.addSubview(draw2D);
+    }
+    
     private func reverseArray(array: Array<Any>, head: Int, tail: Int) -> Array<Any>{
         var results: Array<Any> = array;
-        for i in 0..<(tail-head)/2 {
+        let swapCount: Float = (Float(tail) - Float(head))/2.0;
+        for i in 0..<Int(swapCount.rounded()) {
             results.swapAt(head+i, tail-i);
         }
         return results;
--- ShortestPath/ViewController.swift
+++ ShortestPath/ViewController.swift
@@ -11,55 +11,49 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var pathView: UIView!
+    @IBOutlet weak var runBtn: UIButton!
+    
     var points: [CGPoint] = [];
     
+    var ActivityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
         // Do any additional setup after loading the view.
     }
 
     func putPoint(location: CGPoint) {
         // UIImage インスタンスの生成
-        let manImage:UIImage = UIImage(named:"yokokara_shitsurei")!
+        let manImage:UIImage = UIImage(named:"yokokara_shitsurei")!;
         // UIImageView 初期化
-        let imageView = UIImageView(image:manImage)
+        let imageView = UIImageView(image:manImage);
         // 画像の縦横サイズを取得
-        let imgWidth:CGFloat = manImage.size.width/10
-        let imgHeight:CGFloat = manImage.size.height/10
+        let imgWidth:CGFloat = manImage.size.width/10;
+        let imgHeight:CGFloat = manImage.size.height/10;
         let rect:CGRect =
-            CGRect(x:0, y:0, width:imgWidth, height:imgHeight)
+            CGRect(x:0, y:0, width:imgWidth, height:imgHeight);
         // ImageView frame をCGRectで作った矩形に合わせる
         imageView.frame = rect;
         // 画像の中心を画面の中心に設定
-        imageView.center = CGPoint(x:location.x, y:location.y)
+        imageView.center = CGPoint(x:location.x, y:location.y);
         // UIImageViewのインスタンスをビューに追加
-        self.pathView.addSubview(imageView)
+        self.pathView.addSubview(imageView);
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
-            let location = touch.location(in: pathView)
-            self.putPoint(location: location )
+            let location = touch.location(in: pathView);
+            self.putPoint(location: location );
             self.points.append(location);
-            Swift.print(location)
+            self.runBtn.isEnabled = true;
+            print(location);
         }
     }
     
-    func drawPathLines(resultPaths: [CGPoint] ) {
-        self.view.viewWithTag(999)?.removeFromSuperview();
-        let draw2D: Draw2D = Draw2D(frame: pathView.frame);
-        draw2D.tag = 999;
-        draw2D.isOpaque = false;
-        draw2D.backgroundColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0);
-        draw2D.getResultPaths(resultPaths: resultPaths);
-        print(draw2D.frame);
-        self.view.addSubview(draw2D);
-    }
-    
     @IBAction func runBtn(_ sender: Any) {
-        let travelingSalesman: TravelingSalesman = TravelingSalesman(points: self.points);
+        let travelingSalesman: TravelingSalesman = TravelingSalesman(points: self.points, superView: self.view, pathView: self.pathView);
         let resultPaths = travelingSalesman.execute();
-        self.drawPathLines(resultPaths: resultPaths);
+        travelingSalesman.drawPathLines(resultPaths: resultPaths, superView: self.view, pathView: self.pathView);
     }
     
 }
